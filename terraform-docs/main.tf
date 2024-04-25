b98e0854642ce68533ee7952a9a05bb240b3109e
mestbzts <mexst97@gmail.com>
2024-04-25 03:47:59 +0000

    neww

--- terraform-docs/main.tf
+++ terraform-docs/main.tf
@@ -10,6 +10,64 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "s3-deneme" {
-  bucket = "jenkins-proj-trial-v1"
+locals {
+  instance-type = "t3a.medium"
+  key-name = "Martin-Greystone"
+  secgr-dynamic-ports = [22,5000,3000,5432]
+  user = "insbekir"
+  ami = "ami-0230bd60aa48260c6"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "${local.user}-docker-instance-sg"
+  description = "Allow SSH inbound traffic"
+
+  dynamic "ingress" {
+    for_each = local.secgr-dynamic-ports
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+  egress {
+    description = "Outbound Allowed"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = local.ami
+  instance_type = local.instance-type
+  key_name = local.key-name
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  tags = {
+      Name = "${local.user}-Docker-instance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              hostnamectl set-hostname docker_instance
+              yum update -y
+              yum install docker -y
+              systemctl start docker
+              systemctl enable docker
+              usermod -a -G docker ec2-user
+              newgrp docker
+              # install docker-compose
+              curl -SL https://github.com/docker/compose/releases/download/v2.24.7/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+              chmod +x /usr/local/bin/docker-compose
+	          EOF
+}  
+output "myec2-public-ip" {
+  value = aws_instance.tf-ec2.public_ip
+}
+
+output "ssh-connection-command" {
+  value = "ssh -i ${local.key-name}.pem ec2-user@${aws_instance.tf-ec2.public_ip}"
 }
